58da51a17e18b2b73263ab5ee4438f3676a938eb
Joshua Siagian <suabahasa@gmail.com>
2021-08-08 16:45:26 +0700

    update og meta

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,12 +1,16 @@
 module.exports = {
   base: '/',
   lang: 'en-US',
-  title: 'Collaboration for Oxygen Editor',
+  title: 'Collaboration',
   description: 'Oxygen Editor Multi User and Multi Tab Editing support',
 
   head: [
     ['meta', { property: 'og:title', content: 'Collaboration for Oxygen Editor' }],
+    ['meta', { property: 'og:type', content: 'website' }],
     ['meta', { property: 'og:image', content: 'https://i1.wp.com/i.ytimg.com/vi/q51xhGxTk5k/maxresdefault.jpg?ssl=1' }],
+    ['meta', { property: 'og:image:secure_url', content: 'https://i1.wp.com/i.ytimg.com/vi/q51xhGxTk5k/maxresdefault.jpg?ssl=1' }],
+    ['meta', { property: 'og:image:width', content: '1280' }],
+    ['meta', { property: 'og:image:height', content: '720' }],
     ['meta', { property: 'og:image:alt', content: 'Oxygen Editor Multi User and Multi Tab Editing support' }],
     ['link', { rel: 'icon', href: 'https://i1.wp.com/dplugins.com/wp-content/uploads/2020/10/cropped-dplugins-avatar-1-192x192.png?ssl=1' }],
   ],
